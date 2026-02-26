f9577ebcb5b356d9acfc48921b8ee7dbde691bcb
omersaban <133633504+omersaban@users.noreply.github.com>
2026-02-26 16:17:57 +0200

    Create products.js

---
+++ src/data/products.js
@@ -0,0 +1,23 @@
+export const products = [
+  {
+    id: "business-cards",
+    name: "כרטיסי ביקור",
+    description: "נייר 350 גרם, הדפסה דו-צדדית, למינציה מט/מבריק",
+    basePrice: 50,
+    icon: "CreditCard" 
+  },
+  {
+    id: "flyers",
+    name: "פליירים",
+    description: "הדפסה צבעונית על נייר כרומו 135 גרם",
+    basePrice: 0.2, // מחיר ליחידה
+    icon: "FileText"
+  },
+  {
+    id: "posters",
+    name: "פוסטרים",
+    description: "הדפסה איכותית בפורמט רחב",
+    basePrice: 40,
+    icon: "Image"
+  }
+];
