7b085595f3e793a48e50f945da288b8ffafcc51c
omersaban <133633504+omersaban@users.noreply.github.com>
2026-03-10 12:23:21 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -96,14 +96,14 @@ export default function Home() {
             {/* כפתור הזמנה אונליין - צד ימין (זז ימינה ולמטה) */}
             <Link 
               to="/Order" 
-              className="absolute bottom-[12%] right-[3%] w-[15%] h-[12%] cursor-pointer z-10 hover:bg-white/10 transition-colors rounded-lg"
+              className="absolute bottom-[12%] right-[24%] w-[14%] h-[12%] cursor-pointer z-10 hover:bg-white/10 transition-colors rounded-lg"
               aria-label="הזמנה אונליין"
             />
             
             {/* כפתור למידע נוסף - צד שמאל (זז שמאלה ולמטה) */}
             <Link 
               to="/About" 
-              className="absolute bottom-[12%] right-[18%] w-[10%] h-[13%] cursor-pointer z-10 hover:bg-white/10 transition-colors rounded-lg"
+              className="absolute bottom-[12%] right-[5%] w-[15%] h-[12%] cursor-pointer z-10 hover:bg-white/10 transition-colors rounded-lg"
               aria-label="למידע נוסף"
             />
           </div>
