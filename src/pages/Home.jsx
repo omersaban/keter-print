97b6ecc41ababfb9aab59fdf772252f207c467c4
omersaban <133633504+omersaban@users.noreply.github.com>
2026-03-10 12:14:57 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -93,17 +93,17 @@ export default function Home() {
           />
           
           <div className="absolute inset-0">
-            {/* כפתור הזמנה אונליין - צד ימין (מתוקן) */}
+            {/* כפתור הזמנה אונליין - צד ימין (זז ימינה ולמטה) */}
             <Link 
               to="/Order" 
-              className="absolute bottom-[12%] right-[20%] w-[14%] h-[11%] cursor-pointer z-10 hover:bg-white/10 transition-colors rounded-lg"
+              className="absolute bottom-[12%] right-[8%] w-[15%] h-[12%] cursor-pointer z-10 hover:bg-white/10 transition-colors rounded-lg"
               aria-label="הזמנה אונליין"
             />
             
-            {/* כפתור למידע נוסף - צד שמאל (מתוקן) */}
+            {/* כפתור למידע נוסף - צד שמאל (זז שמאלה ולמטה) */}
             <Link 
               to="/About" 
-              className="absolute bottom-[12%] right-[35%] w-[13%] h-[11%] cursor-pointer z-10 hover:bg-white/10 transition-colors rounded-lg"
+              className="absolute bottom-[12%] right-[29%] w-[14%] h-[12%] cursor-pointer z-10 hover:bg-white/10 transition-colors rounded-lg"
               aria-label="למידע נוסף"
             />
           </div>
